0d6c8bdb842c202c019c4e9cf85c72556050d255
MasayaHiketa <b10902089@ntu.edu.tw>
2025-11-11 23:23:24 +0800

    after edit test

--- backend/src/test/java/com/masaya/PlantWateringSystemApplicationTests.java
+++ backend/src/test/java/com/masaya/PlantWateringSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ma;
+package com.masaya; // ← 本番のApplicationと同じにする
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PlantWateringSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
